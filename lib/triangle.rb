7f90699fdf24f9bd887465cef6f0b651872fc414
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 02:41:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :size1, :size2, :size3
+  
+  def initialize(size1,size2,size3)
+    @size1 = size1
+    @size2 = size2
+    @size3 = size3
+  end
+  
+  def kind 
+    if illegal_triangle == true && no_negative_triangle == true
+      true 
+    else  
+      raise TriangleError
+    end
+    
+    if size1 == size2 && size2 == size3 
+      :equilateral
+    elsif size1 != size2 && size2 != size3 && size1 != size3 
+      :scalene
+    else 
+      :isosceles
+    end
+  end
+    
+    def illegal_triangle
+       size1+size2>size3 && size2+size3>size1 && size3+size1>size2
+    end
+    
+    def no_negative_triangle
+      size1 > 0 && size2 > 0 && size3 > 0 
+    end
+    
+    class TriangleError < StandardError
+    end
 end
